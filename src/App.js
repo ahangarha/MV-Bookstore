33064190dc3b4df03604fb78af0866240ab119fd
Mostafa Ahangarha <ahangarha@riseup.net>
2022-03-23 20:10:30 +0300

    improve global style

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import Books from './pages/Books';
-import Categories from './pages/Categories';
+import BooksPage from './pages/BooksPage';
+import CategoriesPage from './pages/CategoriesPage';
+import './App.css';
 
 export default function App() {
+  const [books] = useState([
+    {
+      title: 'book 1',
+    },
+    {
+      title: 'book 2',
+    },
+  ]);
+
   return (
     <BrowserRouter>
       <Header />
-      <main>
+      <main className="container">
         <Routes>
-          <Route path="/" element={<Books />} />
-          <Route path="/categories" element={<Categories />} />
+          <Route path="/" element={<BooksPage books={books} />} />
+          <Route path="/categories" element={<CategoriesPage />} />
         </Routes>
       </main>
     </BrowserRouter>
