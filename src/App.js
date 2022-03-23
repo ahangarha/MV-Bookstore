77a2cc90159aec4a75d9a371d52e7297db6486f2
Mostafa Ahangarha <ahangarha@riseup.net>
2022-03-23 17:46:32 +0300

    add basic routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Books from './pages/Books';
+import Categories from './pages/Categories';
 
 export default function App() {
   return (
-    <div>
-      the app
-    </div>
+    <BrowserRouter>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
