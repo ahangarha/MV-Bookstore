576c3859fa21557ce8c0375d227ea62cc0c160eb
Mostafa Ahangarha <ahangarha@riseup.net>
2022-03-28 22:15:32 +0300

    connect add book form to redux

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
+import { addBook } from '../redux/books/books';
 import './AddBookForm.css';
 
 export default function AddBookForm({ categories }) {
+  const [bookTitle, setBookTitle] = useState('');
+
+  const updateBookTitle = (e) => {
+    e.preventDefault();
+    setBookTitle(e.target.value);
+  };
+
+  const dispatch = useDispatch();
+
   return (
     <section id="form-section">
       <h2>Add new book</h2>
-      <form id="add-book-form">
-        <input type="text" name="title" required />
+      <form
+        id="add-book-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch(addBook({
+            title: bookTitle.trim(),
+            author: 'Unknown',
+          }));
+          setBookTitle('');
+        }}
+      >
+        <input
+          type="text"
+          name="title"
+          value={bookTitle}
+          onChange={updateBookTitle}
+          placeholder="Book Title"
+          pattern=".*\S+.*"
+          required
+        />
         <select name="category">
           {
             categories.forEach((cat) => (
