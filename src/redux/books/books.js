d1a81c27e68e3019a4efff835a2e2a2f32790b9b
Mostafa Ahangarha <ahangarha@riseup.net>
2022-03-30 19:45:52 +0300

    remove book from API and page

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,6 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const ADD_BOOK = 'bookstore/books/ADD';
 const REMOVE_BOOK = 'bookstore/books/REMOVE';
+const BOOK_REMOVED = 'bookstore/books/BOOK_REMOVE';
 const BOOKS_FETCHED = 'bookstore/books/BOOKS_FETCHED';
 
 export const addBook = (data) => ({
@@ -11,12 +12,31 @@ export const addBook = (data) => ({
   },
 });
 
-export const removeBook = (id) => ({
-  type: REMOVE_BOOK,
+export const bookRemoved = (id) => ({
+  type: BOOK_REMOVED,
   payload: {
     id,
   },
 });
+
+export const removeBook = (id) => (dispatch) => {
+  // call api to delete book with this id
+  let URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/gKNxtZf3AJCaGnNUdIGw/books/';
+  URL += id;
+  const options = {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      book_id: id,
+    }),
+  };
+  fetch(URL, options)
+    .then((res) => {
+      if (res.status === 201) dispatch(bookRemoved(id));
+    });
+};
 
 const booksFetched = (books) => ({
   type: BOOKS_FETCHED,
@@ -49,6 +69,8 @@ export default function reducer(state = [], action) {
         ...action.payload.data,
       });
     case REMOVE_BOOK:
+      return removeBook(action.payload.id);
+    case BOOK_REMOVED:
       return state.filter((book) => book.id !== action.payload.id);
     default:
       return state;
