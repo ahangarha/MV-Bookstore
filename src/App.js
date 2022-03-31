bd2d34975ea839411012ab099a1093743d520ced
Mostafa Ahangarha <ahangarha@riseup.net>
2022-03-31 17:24:38 +0300

    restyle app component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import BooksPage from './pages/BooksPage';
 import CategoriesPage from './pages/CategoriesPage';
-import './App.css';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
-      <main className="container">
+      <main className="container mx-auto my-10">
         <Routes>
           <Route path="/" element={<BooksPage />} />
           <Route path="/categories" element={<CategoriesPage />} />
